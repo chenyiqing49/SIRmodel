b37c76ae14259900738e1e38afa6d09c145ec7e5
Samuele Lanzi <65108938+samuelelanzi@users.noreply.github.com>
2020-08-03 13:03:53 +0200

    Delete main.cpp

--- main.cpp
+++
@@ -1,8 +0,0 @@
-#include<iostream>
-
-int main(){
-    std::cout << "1 + 1 = 2" << '\n';
-    std::cout << "1 * 2 = 2" << '\n';
-    std::cout << "Modifica" << '\n';
-    std::cout << "Modifica desktop" << '\n';
-}
