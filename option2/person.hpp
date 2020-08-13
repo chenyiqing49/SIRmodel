634b83cc8eb55a67868829da3cf4b02333571320
Samuele Lanzi <65108938+samuelelanzi@users.noreply.github.com>
2020-08-13 10:32:16 +0200

    Add files via upload

---
+++ option2/person.hpp
@@ -0,0 +1,49 @@
+#ifndef SIR_PERSON_HPP
+#define SIR_PERSON_HPP
+
+#include <random>
+
+namespace SIR {
+enum class State : char { Susceptible, Infectious, Recovered };
+
+class Person {
+private:
+  int days_;
+  State state_;
+
+public:
+  Person() : days_{0}, state_{State::Susceptible} {}
+
+  State getState() { return state_; }
+  int getDays() { return days_; }
+
+  void update() {
+    if (state_ == State::Infectious) {
+      --days_;
+      if (days_ == 0) {
+        state_ = State::Recovered;
+      }
+    }
+  }
+
+  void infect() {
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<> distrib(7, 14);
+    int days = distrib(gen);
+
+    if (state_ == State::Susceptible) {
+      days_ = days;
+      state_ = State::Infectious;
+    }
+  }
+
+  bool is_S() const { return state_ == State::Susceptible; }
+
+  bool is_I() const { return state_ == State::Infectious; }
+
+  bool is_R() const { return state_ == State::Recovered; }
+};
+} // namespace SIR
+
+#endif
