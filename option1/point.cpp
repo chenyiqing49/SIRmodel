924d2b4bfeb86bd886e85d3eaa79b40702fe9762
Samuele Lanzi <65108938+samuelelanzi@users.noreply.github.com>
2020-08-19 17:43:25 +0200

    Update point.cpp

--- option1/point.cpp
+++ option1/point.cpp
@@ -1,10 +1,12 @@
 #include "point.hpp"
 
+// overload dell'operatore ==
 bool operator==(Point const& l, Point const& r)
 {
   return l.x == r.x && l.y == r.y;
 }
 
+// overload dell'operatore !=
 bool operator!=(Point const& l, Point const& r)
 {
   return !(l == r);
