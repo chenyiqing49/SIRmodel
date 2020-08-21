c8c083bb9c1d79124bc747052cdbd14ef7d05bbf
Samuele Lanzi <65108938+samuelelanzi@users.noreply.github.com>
2020-08-21 10:55:41 +0200

    Update main.cpp

--- option2/main.cpp
+++ option2/main.cpp
@@ -21,12 +21,12 @@ int main() {
   std::cout << "Press any key to start the animation.\n";
   display.wait_key_pressed();
 
-  constexpr int d = 500;
+  // constexpr int d = 500;
 
-  constexpr int window_width = 600;
-  constexpr int window_height = 600;
+  constexpr int window_width = 700;
+  constexpr int window_height = 700;
   constexpr int point_min_x = 0;
-  constexpr int point_max_x = d;
+  constexpr int point_max_x = 600;
   constexpr double point_min_y = 0;
   constexpr double point_max_y = 48400;
 
@@ -71,51 +71,60 @@ int main() {
       window1.draw(zero);
     }
 
-    for (int i = 0; i != d; ++i) {
+    for (int i = 0; i != 600; ++i) {
       std::cout << "\033c";
-      std::cout << "S: " << count_S(board) << '\t' << "I: " << count_I(board) << "\t\t" << "R: " << count_R(board) << '\n';
+      std::cout << "S: " << count_S(board) << '\n'
+                << "I: " << count_I(board) << '\n'
+                << "R: " << count_R(board) << '\n';
 
-      SIR::Point p2{i, count_S(board)};
-      pointsS.push_back(p2);
+      SIR::Point p1{i, count_S(board)};
+      pointsS.push_back(p1);
 
-      SIR::Point p{i, count_I(board)};
-      pointsI.push_back(p);
+      SIR::Point p2{i, count_I(board)};
+      pointsI.push_back(p2);
 
-      SIR::Point p1{i, count_R(board)};
-      pointsR.push_back(p1);
+      SIR::Point p3{i, count_R(board)};
+      pointsR.push_back(p3);
 
       board = evolve(board);
       display.draw(board);
 
       for (auto const &p : pointsS) {
         constexpr auto radius = 2.f;
+        sf::CircleShape c1{radius};
+        c1.setFillColor(sf::Color::Red);
+        c1.setOrigin(sf::Vector2f{radius, radius});
+        c1.move(to_window_frame(p));
+        window1.draw(c1);
+      }
+
+      for (auto const &p : pointsI) {
+        constexpr auto radius = 2.f;
         sf::CircleShape c2{radius};
-        c2.setFillColor(sf::Color::Red);
+        c2.setFillColor(sf::Color::Green);
         c2.setOrigin(sf::Vector2f{radius, radius});
         c2.move(to_window_frame(p));
         window1.draw(c2);
       }
 
-      for (auto const &p : pointsI) {
-        constexpr auto radius = 2.f;
-        sf::CircleShape c{radius};
-        c.setFillColor(sf::Color::Green);
-        c.setOrigin(sf::Vector2f{radius, radius});
-        c.move(to_window_frame(p));
-        window1.draw(c);
-      }
-
       for (auto const &p : pointsR) {
         constexpr auto radius = 2.f;
-        sf::CircleShape c1{radius};
-        c1.setFillColor(sf::Color::Blue);
-        c1.setOrigin(sf::Vector2f{radius, radius});
-        c1.move(to_window_frame(p));
-        window1.draw(c1);
+        sf::CircleShape c3{radius};
+        c3.setFillColor(sf::Color::Blue);
+        c3.setOrigin(sf::Vector2f{radius, radius});
+        c3.move(to_window_frame(p));
+        window1.draw(c3);
       }
-      
+
       window1.display();
-      std::this_thread::sleep_for(std::chrono::milliseconds(50));
+      std::this_thread::sleep_for(std::chrono::milliseconds(30));
+    }
+    sf::Event event;
+    while (window1.waitEvent(event)) {
+      if (event.type == sf::Event::Closed) {
+        window1.close();
+        break;
+      }
     }
   }
 }
