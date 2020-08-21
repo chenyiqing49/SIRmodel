17de75fc062cbf4a17729cd2b2f4e5f47bf302f3
Samuele Lanzi <65108938+samuelelanzi@users.noreply.github.com>
2020-08-21 09:19:13 +0200

    Update sir.cpp

--- option1/sir.cpp
+++ option1/sir.cpp
@@ -1,20 +1,21 @@
 #include "sir.hpp"
 #include <cassert>
 
-// declarations and definitions of the member functions
+// Declaration and definition of member functions
 SIR Epidemy::getInit() { return init_; }
 
 int Epidemy::getNday() { return n_day_; }
 
 std::vector<SIR> Epidemy::create_model(double const &beta, double const &gamma, int const &N) {
   assert(n_day_ > 0);
+  assert(beta > 0);
+  assert(gamma > 0);
 
   std::vector<SIR> result{init_};
 
   for (int i = 1; i < n_day_; ++i) {
     auto const &prev = result.back();
     SIR s;
-    // SIR model's formulas
     s.susceptible =
         prev.susceptible - beta * prev.infectious * prev.susceptible;
     s.infectious = prev.infectious + beta * prev.infectious * prev.susceptible -
