24ae7c819ba93cd9076a185d3673f36efca12319
samuelemerola <69164971+samuelemerola@users.noreply.github.com>
2020-08-30 16:03:47 +0200

    Update sir.hpp

--- option1/sir.hpp
+++ option1/sir.hpp
@@ -15,13 +15,14 @@ private:
   int n_day_;
 
 public:
-  Epidemy(SIR init, int n_day) : init_{init}, n_day_{n_day} { }
-  
+  Epidemy(SIR init, int n_day) : init_{init}, n_day_{n_day} {}
+
   // declarations of the member functions
   SIR getInit();
   int getNday();
-  
-  std::vector<SIR> create_model(double const &beta, double const &gamma, int const &N);
+
+  std::vector<SIR> create_model(double const &beta, double const &gamma,
+                                int const &N);
 };
 
 #endif
