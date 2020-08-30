c74eb2d1630206eefc2e82378782974c84f31b62
Samuele Lanzi <65108938+samuelelanzi@users.noreply.github.com>
2020-08-30 15:43:35 +0200

    Update main.cpp

--- option2/main.cpp
+++ option2/main.cpp
@@ -16,17 +16,18 @@ int main() {
   std::cout << "Insert infect cells with mouse's left button...\n";
   std::cout << "Press any key to start the animation\n";
 
+  int const c_size = display.getCellSize();
+
   // Infect cells clicked by mouse
   while (display.wait_key() == true) {
-    int const c_size = display.getCellSize();
-    display.draw(board);
     auto p1 = display.getMousePosition();
     int X = p1.x / c_size;
     int Y = p1.y / c_size;
     board(Y, X).infectSure();
+    display.draw(board);
   }
 
-  constexpr int d = 1000;
+  constexpr int d = 800;
 
   constexpr int window_width = d + 20;
   constexpr int window_height = d + 20;
@@ -39,8 +40,8 @@ int main() {
   sf::ContextSettings settings;
   settings.antialiasingLevel = 8;
 
-  sf::RenderWindow window1(sf::VideoMode(window_width, window_height),
-                           "SIR graphs", sf::Style::Default, settings);
+  sf::RenderWindow window(sf::VideoMode(window_width, window_height),
+                          "SIR graphs", sf::Style::Default, settings);
 
   // (lambda) function to convert a point to the coordinates of the SFML window
   auto to_window_frame = [](SIR::Point const &p) {
@@ -57,8 +58,8 @@ int main() {
                             (p.y - point_min_y) * y_scale);
   };
 
-  if (window1.isOpen()) {
-    window1.clear(sf::Color::Black);
+  while (window.isOpen()) {
+    window.clear(sf::Color::Black);
 
     {
       // set the origin
@@ -70,15 +71,13 @@ int main() {
       zero.setOutlineColor(sf::Color::White);
       zero.setOrigin(sf::Vector2f{radius_O, radius_O});
       zero.move(to_window_frame({0, 0}));
-      window1.draw(zero);
+      window.draw(zero);
     }
 
-    // vector that contains S and R values
     std::vector<int> s{N};
     std::vector<int> r{0};
 
     for (int i = 0; i != d; ++i) {
-      // search the previous value
       int const &prevS = s.back();
       int const &prevR = r.back();
 
@@ -89,8 +88,6 @@ int main() {
       s.push_back(countS);
       r.push_back(countR);
 
-      // through the formulas of the SIR model we obtain
-      // the values of beta, gamma and R_0
       double b_num = static_cast<double>(prevS - countS);
       double b_den = static_cast<double>(countS * countI);
       double beta = b_num / b_den;
@@ -101,7 +98,7 @@ int main() {
 
       double R_0 = N * (beta / gamma);
 
-      // print S, I, R, beta, gamma and R_0 values on user's console
+      // print S, I, and R values on user's console
       std::cout << "\033c";
       std::cout << "S: " << countS << '\n'
                 << "I: " << countI << '\n'
@@ -111,45 +108,45 @@ int main() {
                 << "R_0: " << R_0 << '\n';
 
       if (countI == 0) {
+        std::cout << "Infectious are equal to zero, nothing else to do\n";
         break;
-      } else {
-        // define three points
-        SIR::Point p1{i, countS};
-        SIR::Point p2{i, countI};
-        SIR::Point p3{i, countR};
-
-        board = evolve(board);
-        display.draw(board);
-
-        // draw points on graph window
-        constexpr auto radius = 2.f;
-
-        sf::CircleShape c1{radius};
-        c1.setFillColor(sf::Color::Red);
-        c1.setOrigin(sf::Vector2f{radius, radius});
-        c1.move(to_window_frame(p1));
-        window1.draw(c1);
-
-        sf::CircleShape c2{radius};
-        c2.setFillColor(sf::Color::Green);
-        c2.setOrigin(sf::Vector2f{radius, radius});
-        c2.move(to_window_frame(p2));
-        window1.draw(c2);
-
-        sf::CircleShape c3{radius};
-        c3.setFillColor(sf::Color::Blue);
-        c3.setOrigin(sf::Vector2f{radius, radius});
-        c3.move(to_window_frame(p3));
-        window1.draw(c3);
-
-        window1.display();
-        std::this_thread::sleep_for(std::chrono::milliseconds(30));
       }
+
+      SIR::Point p1{i, countS};
+      SIR::Point p2{i, countI};
+      SIR::Point p3{i, countR};
+
+      board = evolve(board);
+      display.draw(board);
+
+      // draw points on graph window
+      constexpr auto radius = 1.f;
+
+      sf::CircleShape c1{radius};
+      c1.setFillColor(sf::Color::Red);
+      c1.setOrigin(sf::Vector2f{radius, radius});
+      c1.move(to_window_frame(p1));
+      window.draw(c1);
+
+      sf::CircleShape c2{radius};
+      c2.setFillColor(sf::Color::Green);
+      c2.setOrigin(sf::Vector2f{radius, radius});
+      c2.move(to_window_frame(p2));
+      window.draw(c2);
+
+      sf::CircleShape c3{radius};
+      c3.setFillColor(sf::Color::Blue);
+      c3.setOrigin(sf::Vector2f{radius, radius});
+      c3.move(to_window_frame(p3));
+      window.draw(c3);
+
+      window.display();
+      std::this_thread::sleep_for(std::chrono::milliseconds(30));
     }
     sf::Event event;
-    while (window1.waitEvent(event)) {
+    while (window.waitEvent(event)) {
       if (event.type == sf::Event::Closed) {
-        window1.close();
+        window.close();
         break;
       }
     }
